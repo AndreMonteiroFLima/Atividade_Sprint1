8a4018155cd473669e8a9b3e810bdc23af97ff40
André Monteiro <52679448+andre.m.f.lima@outlook.com>
2022-02-03 20:03:08 -0300

    inserido código de serial recomendado pela IDE para tirar warning

--- src/atividade7/LoginFailException.java
+++ src/atividade7/LoginFailException.java
@@ -2,6 +2,11 @@ package atividade7;
 
 public class LoginFailException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public LoginFailException(String msg) {
 		super(msg);
 	}
